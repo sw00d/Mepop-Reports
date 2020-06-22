d87a6d04426aa57e207dbe3c5f8de1ed1f16fb1f
Samuel Wood <samuelwood@macbook-pro.home>
2020-06-22 15:09:49 -0600

    Added better nav highlighting in header

--- packages/mepop-frontend/src/site/main/Header.js
+++ packages/mepop-frontend/src/site/main/Header.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { container, logo, navigation, navBtn } from '../styles/header.module.scss'
+import { container, logo, navigation, navBtn, activeNavBtn } from '../styles/header.module.scss'
 
 const Header = () => {
+  const [activeTab, setActiveTab] = useState(window.location.pathname)
+
   return (
     <div className={container}>
-      <h1 className={logo}><i>Mepop</i></h1>
+      <Link to='/' onClick={() => setActiveTab('/')}>
+        <h1 className={logo}><i>Mepop</i></h1>
+      </Link>
       <div className={navigation}>
-        <Link className={navBtn} to='/'>Reports</Link>
-        <Link className={navBtn} to='/about'>About</Link>
+        <Link
+          className={activeTab === '/' ? activeNavBtn : navBtn}
+          onClick={() => setActiveTab('/')}
+          to='/'
+        >Reports
+        </Link>
+        <Link
+          className={activeTab === '/about' ? activeNavBtn : navBtn}
+          onClick={() => setActiveTab('/about')}
+          to='/about'
+        >About
+        </Link>
       </div>
     </div>
   )
